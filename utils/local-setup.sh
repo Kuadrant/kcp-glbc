77f3c735146cbe0d266393d98c238509e57dbd36
Phil Brookes <phil.brookes@gmail.com>
2022-07-04 09:22:42 +0100

    Update local-setup.sh

--- utils/local-setup.sh
+++ utils/local-setup.sh
@@ -117,8 +117,9 @@ createWorkloadCluster() {
 
   kubectl config use-context kind-${1}
 
-  echo "Deploying Ingress controller to ${1}"
-  curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml | sed "s/--publish-status-address=localhost/--report-node-internal-ip-address/g" | kubectl apply -f -
+  echo "Deploying Ingress controller to ${1}"  
+  VERSION=controller-v1.2.1
+  curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/kind/deploy.yaml | sed "s/--publish-status-address=localhost/--report-node-internal-ip-address/g" | kubectl apply -f -
   kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class=true"
   echo "Waiting for deployments to be ready ..."
   kubectl -n ingress-nginx wait --timeout=300s --for=condition=Available deployments --all
